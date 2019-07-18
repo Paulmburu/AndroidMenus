ffe91a1a317e322e124468de72dafc4ecded2e8d
Paulmburu <paulmburu53@gmail.com>
2019-07-18 12:59:08 +0300

    action mode contextual menu

--- app/src/main/java/com/example/androidmenus/MainActivity.java
+++ app/src/main/java/com/example/androidmenus/MainActivity.java
@@ -2,14 +2,54 @@ package com.example.androidmenus;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.view.ActionMode;
 import android.view.ContextMenu;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    ActionMode mActionMode;
+
+    private ActionMode.Callback mActionModecallback = new ActionMode.Callback() {
+        @Override
+        public boolean onCreateActionMode(ActionMode mode, Menu menu) {
+            getMenuInflater().inflate(R.menu.actionmode_context_menu,menu);
+            return true;
+        }
+
+        @Override
+        public boolean onPrepareActionMode(ActionMode mode, Menu menu) {
+            return false;
+        }
+
+        @Override
+        public boolean onActionItemClicked(ActionMode mode, MenuItem item) {
+            switch (item.getItemId()){
+                case R.id.actionmode_item_1:
+                    Toast.makeText(MainActivity.this, "actionmode_item_1", Toast.LENGTH_SHORT).show();
+                    mode.finish();
+                    return true;
+                case R.id.actionmode_item_2:
+                    Toast.makeText(MainActivity.this, "actionmode_item_1", Toast.LENGTH_SHORT).show();
+                    mode.finish();
+                    return true;
+                default:
+                    return false;
+
+            }
+        }
+
+        @Override
+        public void onDestroyActionMode(ActionMode mode) {
+            mActionMode = null;
+
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +58,21 @@ public class MainActivity extends AppCompatActivity {
 
         TextView tv= (TextView)findViewById(R.id.hello_id);
         this.registerForContextMenu(tv);
+
+        Button btn = (Button) findViewById(R.id.btn);
+        btn.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+
+
+                if(mActionMode != null){
+                    return false;
+                }
+
+                mActionMode = startSupportActionMode(mActionModecallback);
+                return true;
+            }
+        });
     }
 
 
